3a5b484125eff125e092de906c9c04287504dc7f
Isa Maharramov <isa123meherremov@gmail.com>
2026-04-03 02:20:19 +0400

    2:20

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
+    """Prints numbers 1 to 100 with FizzBuzz logic."""
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end="")
@@ -9,7 +10,5 @@ def fizzbuzz():
             print("Buzz", end="")
         else:
             print("{}".format(i), end="")
-        
-        if i < 100:
-            print(" ", end="")
-    print()
+
+        print(" ", end="")
