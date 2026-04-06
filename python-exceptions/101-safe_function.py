f06eecdbce7dedfd922b87c131085281de0131a6
Isa Maharramov <isa123meherremov@gmail.com>
2026-04-06 04:52:04 +0400

    4:52

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import sys
-
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as e:
-        # Use print with file=sys.stderr to ensure correct stream handling
-        print("Exception: {}".format(e), file=sys.stderr)
+        sys.stderr.write("Exception: {}\n".format(e))
         return None
