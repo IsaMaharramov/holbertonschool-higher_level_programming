8473bb05faf9e492229d590c07e44d48db6290f5
Isa Maharramov <isa123meherremov@gmail.com>
2026-04-06 04:43:28 +0400

    4:43

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    """Executes a function safely.
+
+    Args:
+        fct: A pointer to the function to be executed.
+        *args: Variable arguments to pass to fct.
+
+    Returns:
+        The result of the function if successful.
+        None if an exception occurs, with the error printed to stderr.
+    """
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        return None
