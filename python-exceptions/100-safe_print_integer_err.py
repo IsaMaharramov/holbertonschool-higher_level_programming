51c4eef7023a2ab2256cd661aa77621b15805efd
Isa Maharramov <isa123meherremov@gmail.com>
2026-04-06 05:00:51 +0400

    5:00

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,14 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-
     try:
-        # Try to convert the value to an integer and prints it
-        print("{:d}".format(int(value)))
-        # Returns True to indicate that the value was printed correctly
+        print("{:d}".format(value))
         return True
-    except ValueError:
-        # On error, print an error message to stderr
-        print("Exception: Cannot print as integer", file=sys.stderr)
-        # Return False to indicate that the value was not printed correctly
+    except (TypeError, ValueError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
